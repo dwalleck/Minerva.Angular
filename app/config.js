311e8f77dba7d7b32b0305ffa3e7c0183118aa2e
Daryl Walleck <daryl.walleck@rackspace.com>
2015-04-18 19:07:14 -0500

    Refactored MainCtrl into an IIFE

--- app/config.js
+++ app/config.js
@@ -6,21 +6,6 @@ function config($stateProvider, $urlRouterProvider) {
             templateUrl: "app/index.html",
             data: { pageTitle: 'index' }
         })
-        .state('pages', {
-            abstract: true,
-            url: "/pages",
-            templateUrl: "app/common.html",
-        })
-        .state('pages.blank_page', {
-            url: "/blank_page",
-            templateUrl: "app/blank-page.html",
-            data: { pageTitle: 'Blank page' }
-        })
-         .state('pages.blank_page2', {
-            url: "/blank_page2",
-            templateUrl: "views/blank-page2.html",
-            data: { pageTitle: 'Another blank page' }
-        })
 }
 angular
     .module('minerva')
--- app/controllers.js
+++ app/controllers.js
@@ -1,11 +1,31 @@
-app.controller('MainCtrl', function($scope) {
+/*app.controller('MainCtrl', function($scope) {
 	this.projectBoldName = 'App';
 	this.projectName = 'Name';
     this.userName = 'Example user';
     this.headerText = 'AngularJS Starter Project';
     this.descriptionText = 'Here you can quickly bootstrap your AngularJS project.';
 
-});
+});*/
+
+(function () {
+    "use strict";
+
+    angular
+        .module('minerva')
+        .controller('MainCtrl',
+                     MainCtrl)
+
+    function MainCtrl($scope) {
+        var vm = this;
+
+        vm.projectBoldName = 'App';
+        vm.projectName = 'Name';
+        vm.userName = 'Example user';
+        vm.headerText = 'AngularJS Starter Project';
+        vm.descriptionText = 'Here you can quickly bootstrap your AngularJS project.';
+    }
+})();
+
 
 
 
